9749df711faa8b09f3961d9cbd7192f40d0e63d4
digitalby <yury@digitalby.me>
2019-12-18 14:03:02 +0300

    Initial commit

---
+++ app/src/main/java/me/digitalby/lr5/MainActivity.kt
@@ -0,0 +1,14 @@
+package me.digitalby.lr5
+
+import android.content.pm.ActivityInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+    }
+}
