c7e810c10bfcbd746e13394e0c5523dbd804b720
digitalby <yury@digitalby.me>
2019-12-18 18:36:52 +0300

    Create UI

---
+++ app/src/main/java/me/digitalby/lr5/CreateGameFragment.kt
@@ -0,0 +1,24 @@
+package me.digitalby.lr5
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CreateGameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_game, container, false)
+    }
+
+
+}
--- app/src/main/java/me/digitalby/lr5/LoginActivity.kt
+++ app/src/main/java/me/digitalby/lr5/LoginActivity.kt
@@ -4,11 +4,11 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     }
 }
